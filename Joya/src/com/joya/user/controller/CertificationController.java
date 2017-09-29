9d06336efc801f53481542195e852418e607dba0
Myeongseung <ayd07@naver.com>
2017-09-29 15:08:41 +0900

    로그인 ㄴ이런이러ㅣㅈ다ㅓ긷접ㄷ자ㅣ거바

--- Joya/src/com/joya/user/controller/CertificationController.java
+++ Joya/src/com/joya/user/controller/CertificationController.java
@@ -15,24 +15,24 @@ import com.joya.user.service.UserService;
 import com.joya.user.service.UserServiceImpl;
 
 public class CertificationController implements Controller {
-	
+
 	private UserService userService = new UserServiceImpl();
-	
+
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, UnsupportedEncodingException {
 		ModelAndView mav = new ModelAndView();
 		String location = "/index.joya";
-		
+
 		// 로그인
 		if(request.getMethod().equalsIgnoreCase("post")) {
 			String email = request.getParameter("email");
 			String passwd = request.getParameter("passwd");
 			String referer = request.getParameter("referer");
-			
+
 			User user = userService.isMember(email, passwd);
 			if(user != null){
-			    String userInfo = null;
+				String userInfo = null;
 				try {
 					userInfo = URLEncoder.encode(user.getEmail() + "," + user.getName(), "utf-8");
 				} catch (UnsupportedEncodingException e) {
@@ -41,28 +41,30 @@ public class CertificationController implements Controller {
 				Cookie loginCookie = new Cookie("user", userInfo);
 				loginCookie.setPath("/");
 				response.addCookie(loginCookie);
-			    if(referer != null)  location = referer;
-			}else if(referer.contains("/user/login.joya")) {
-				location = "/index.joya";
-			} else {
-				location = "/user/login.joya";
-			}
-		}else {// 로그아웃
-			String user = null;
-			Cookie[] cookies = request.getCookies();
-			if (cookies != null) {
-				for (Cookie cookie : cookies) {
-					if (cookie.getName().equalsIgnoreCase("user")) {
-						cookie.setMaxAge(0);
-						cookie.setPath("/");
-						response.addCookie(cookie);
-						break;
+				if(referer != null) {
+					if(referer.contains("/user/login.joya")) {
+						location = "/index.joya";
+					} else {
+						location = referer;
 					}
+					}
+				} else {
+					location = "/index.joya";
 				}
-			}
+			}else {// 로그아웃
+				Cookie[] cookies = request.getCookies();
+				if (cookies != null) {
+					for (Cookie cookie : cookies) {
+						if (cookie.getName().equalsIgnoreCase("user")) {
+							cookie.setMaxAge(0);
+							cookie.setPath("/");
+							response.addCookie(cookie);
+							break;
+						}
+					}
+				} 
 		}
 		mav.setView("redirect:"+request.getContextPath() + location);
 		return mav;
 	}
-
 }
