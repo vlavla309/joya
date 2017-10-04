312772ba1ae514e25115405fe4a4d573dab44ab5
vlavla <khj309@naver.com>
2017-10-04 15:28:17 +0900

    구분자 클래스 추가

---
+++ Joya/src/com/joya/common/web/Delimiter.java
@@ -0,0 +1,8 @@
+package com.joya.common.web;
+
+public class Delimiter {
+	public final static String USER_INFO="###";
+	public final static String CART_SIZE="###";
+	public final static String CART_ITEM="@@@";
+	public final static String CART_ITEM_INFO="#@#";
+}
