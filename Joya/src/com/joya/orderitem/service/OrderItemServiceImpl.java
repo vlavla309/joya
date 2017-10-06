bcde3c4a66e03a245ddc3e6db81d5397ee3a1314
vlavla <khj309@naver.com>
2017-10-06 14:56:18 +0900

    OrderItem관련 도메인 생성

---
+++ Joya/src/com/joya/orderitem/service/OrderItemServiceImpl.java
@@ -0,0 +1,31 @@
+package com.joya.orderitem.service;
+
+import java.util.List;
+
+import com.joya.common.db.DaoFactory;
+import com.joya.orderitem.dao.JdbcOrderItemDao;
+import com.joya.orderitem.domain.OrderItems;
+
+public class OrderItemServiceImpl implements OrderItemService {
+	JdbcOrderItemDao itemDao=(JdbcOrderItemDao) DaoFactory.getInstance().getDao(JdbcOrderItemDao.class);
+	@Override
+	public void create(OrderItems orderItem) {
+		itemDao.create(orderItem);
+	}
+
+	@Override
+	public void update(OrderItems orderItem) {
+		itemDao.update(orderItem);
+	}
+
+	@Override
+	public List<OrderItems> listByOrderID(int orderId) {
+		return itemDao.listByOrderID(orderId);
+	}
+
+	@Override
+	public OrderItems search(int orderid, int productid) {
+		return itemDao.search(orderid, productid);
+	}
+
+}
